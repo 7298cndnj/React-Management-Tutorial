d3c9d04c9cba587aaa3d691c8dcbc4c3d5920abb
7298cndnj <7298cndnj@naver.com>
2021-09-22 01:15:22 +0900

    Material 사용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import { Paper } from '@mui/material';
+import { Table } from '@mui/material';
+import { TableHead } from '@mui/material';
+import { TableBody } from '@mui/material';
+import { TableRow } from '@mui/material';
+import { TableCell } from '@mui/material';
+import { withStyles } from '@material-ui/styles';
+
+
+const styles = ({
+  root: {
+  width: "100%",
+  spacing: 3 ,
+  overflowX: "auto"
+  },
+  table: {
+  minWidth: 1080
+  }
+  });
+  
 
 const customers = [{
   'id': 1,
@@ -12,7 +32,7 @@ const customers = [{
 },
 {
   'id': 2,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/animals',
   'name': '문현상',
   'birthday': '931128',
   'gender': '남자',
@@ -20,7 +40,7 @@ const customers = [{
 },
 {
   'id': 3,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/nature',
   'name': '홍길동',
   'birthday': '961222',
   'gender': '남자',
@@ -30,12 +50,22 @@ const customers = [{
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer
+      <Paper className={classes.root}>
+        <Table className={classes.table}> 
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+            </TableHead>
+          <TableBody>
+        {customers.map(c => { return ( <Customer
                 key={c.id}
                 id={c.id}
                 image={c.image}
@@ -45,13 +75,15 @@ class App extends Component {
                 job={c.job}
 
               />
-
             )
           })
         }
-      </div>
+        </TableBody>
+     
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
